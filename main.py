d1d92df10ff854dc269a01db358d349da7e14cbd
kok4444 <koba4444@gmail.com>
2023-05-12 00:31:04 +0300

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def parse(url):
+"""
+Using Selenium library parse screen values
+
+"""
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    parse('https://csfail.live/en/crash')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
